b89799807ad8cacec7e83f208287fd5d5a0741b9
email <jjazminclrk@gmail.com>
2020-10-18 19:24:36 -0400

    adding time blocks to javascript file

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
-theDay=[
-    
+// creating a string of time blocks 
+var theDay = [
+    {
+        id: "zero",
+        hour: "09",
+        Time: "09",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "one",
+        hour: "10",
+        Time: "10",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "two",
+        hour: "11",
+        Time: "11",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "three",
+        hour: "12",
+        Time: "12",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "four",
+        hour: "01",
+        Time: "13",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "five",
+        hour: "02",
+        Time: "14",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "six",
+        hour: "03",
+        Time: "15",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "seven",
+        hour: "04",
+        Time: "16",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "eight",
+        hour: "05",
+        Time: "17",
+        meridiem: "pm",
+        reminder: "",
+    }
 ]
+
