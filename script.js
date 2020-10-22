9758a30ae073aa1e01696dc9da893cc0dc250168
email <jjazminclrk@gmail.com>
2020-10-22 00:05:26 -0400

    added time

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var theDay = [
     {
         id: "zero",
         hour: "09",
+        hr: 9,
         Time: "09",
         meridiem: "am",
         reminder: ""
@@ -11,6 +12,7 @@ var theDay = [
     {
         id: "one",
         hour: "10",
+        hr: 10,
         Time: "10",
         meridiem: "am",
         reminder: ""
@@ -18,6 +20,7 @@ var theDay = [
     {
         id: "two",
         hour: "11",
+        hr: 11,
         Time: "11",
         meridiem: "am",
         reminder: ""
@@ -25,6 +28,7 @@ var theDay = [
     {
         id: "three",
         hour: "12",
+        hr: 12,
         Time: "12",
         meridiem: "pm",
         reminder: ""
@@ -32,6 +36,7 @@ var theDay = [
     {
         id: "four",
         hour: "01",
+        hr: 13,
         Time: "13",
         meridiem: "pm",
         reminder: ""
@@ -39,6 +44,7 @@ var theDay = [
     {
         id: "five",
         hour: "02",
+        hr: 14,
         Time: "14",
         meridiem: "pm",
         reminder: ""
@@ -46,6 +52,7 @@ var theDay = [
     {
         id: "six",
         hour: "03",
+        hr: 15,
         Time: "15",
         meridiem: "pm",
         reminder: ""
@@ -53,6 +60,7 @@ var theDay = [
     {
         id: "seven",
         hour: "04",
+        hr: 16,
         Time: "16",
         meridiem: "pm",
         reminder: ""
@@ -60,15 +68,35 @@ var theDay = [
     {
         id: "eight",
         hour: "05",
+        hr: 17,
         Time: "17",
         meridiem: "pm",
         reminder: "",
     }
 ]
-$(document.ready(function () {
+$(document).ready(function () {
     // targeting the current date in header 
-    var currentDate = moment().format("dddd, " + "MMMM Do YYYY");
+    var currentDate = moment().format("MM-DD-YYYY");
     $('#currentDay').text(currentDate);
+    theDay.map(function (hour) {
+        var thisHour = parseInt(moment().format('h'));
+        console.log(thisHour, hour.hr)
+        if (thisHour < hour.hr) {
+            $(`#${hour.id}`).addClass("past")
+        }
+        else if (thisHour === hour.hr) {
+            $(`#${hour.id}`).addClass("present")
+        }
+        else (thisHour)
+        $(`#${hour.id}`).addClass("future")
+    })
 
-    console.log(currentDate)
-}))
+    // console.log(currentDate)
+    var saveButton = $("<i class=' far fa-save-fa-lg'></i>");
+    var savePlan = $("<button>")
+        .attr({
+            "class": "col-md-1 saveBtn"
+        },
+            savePlan.append(saveButton),
+
+    )})
